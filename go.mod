2611cefe49cb93db324d208695055da0f41f8590
内田圭亮 <keisuke-u@weseek.co.jp>
2021-10-10 03:11:53 +0900

    user create

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kagi-opendoor222/go_practice3
 go 1.17
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/kagi-opendoor222/go_practice3/app/models"
 )
@@ -13,4 +14,18 @@ func main() {
 	// fmt.Println(config.Config.Logfile)
 
 	fmt.Println(models.Db)
+
+	// u := &models.User{}
+	// u.Name = "testman"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, err := models.GetUser(1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(u)
 }
