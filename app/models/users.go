d5f9488c523353612a54c60a18586f9cfeed4f55
内田圭亮 <keisuke-u@weseek.co.jp>
2021-10-18 12:03:40 +0900

    ユーザー機能の充実

--- app/models/users.go
+++ app/models/users.go
@@ -49,3 +49,19 @@ func GetUser(id int) (u User, err error) {
 	)
 	return user, err
 }
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `update users set name = ?, email = ? where id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+
+	return err
+}
